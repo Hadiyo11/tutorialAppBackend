0ac5a4eec110c34f5c0bea09cae77e24a30a2530
Hadiyo11 <hadyhope@gmail.com>
2023-10-30 10:31:20 -0700

    new work

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express()
 
 var corsOptions = {
-    origin: "https://6537815892ff160087109dbc--glowing-genie-6feaf5.netlify.app"
+    origin: "https://tutorials_app.netlify.app"
 };
 
 app.use(cors(corsOptions));
